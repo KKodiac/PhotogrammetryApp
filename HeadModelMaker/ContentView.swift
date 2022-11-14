161946745e975f5f9e6dfd8ea27158408eb1a975
홍성민 <seanhong2000@gmail.com>
2022-11-14 10:12:32 +0900

    Initial Commit

---
+++ HeadModelMaker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HeadModelMaker
+//
+//  Created by Sean Hong on 2022/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeadModelMaker/HeadModelMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadModelMakerApp.swift
+//  HeadModelMaker
+//
+//  Created by Sean Hong on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct HeadModelMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
